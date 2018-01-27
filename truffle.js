b6d36994e3700282a80b2b14ea3e63accc6d7145
Unknown <akustov@student.ethz.ch>
2018-01-27 22:59:41 +0100

    x

--- truffle.js
+++ truffle.js
@@ -1,4 +1,23 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 9545,
+      network_id: "*" // Match any network id
+    },
+    ganesh: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x58d78e178A750AD82dB172fE7a8017CaA7d084E4", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
 };
