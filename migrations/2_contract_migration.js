367bfbcfa7434ef147a88c9a237fd633a73260e5
Unknown <akustov@student.ethz.ch>
2018-01-28 14:56:01 +0100

    и

--- migrations/2_contract_migration.js
+++
@@ -1,5 +0,0 @@
-var TittyPurchase = artifacts.require("TittyPurchase");
-
-module.exports = function(deployer) {
-    deployer.deploy(TittyPurchase, '0xA2b3737E984f65bb94ED7ebc96cE9A6e243133dC', '0x0bA6a237d09a420b265ADC0719b7Eb558F1096a3');
-  };
--- truffle.js
+++ truffle.js
@@ -1,23 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    },
-    ganesh: {
-      host: "localhost",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
-    rinkeby: {
-      host: "localhost", // Connect to geth on the specified
-      port: 8545,
-      from: "0x58d78e178A750AD82dB172fE7a8017CaA7d084E4", // default address to use for any transaction Truffle makes during migrations
-      network_id: 4,
-      gas: 4612388 // Gas limit used for deploys
+      network_id: "*", // Match any network id
+      // from: "0xf86c5833419fd517e38179519ccf102155d48952"
     }
   }
 };
